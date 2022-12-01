6719f4fb40dc6a287d51f27a37170f74392ca554
37h4nn4ug13 <nnaugle1@vols.utk.edu>
2022-12-01 16:14:12 -0500

    Major style changes

--- urgency/urls.py
+++ urgency/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('changeprogress', changeProgress),
     path('remove', remove),
     path('accounts/', include("users.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
